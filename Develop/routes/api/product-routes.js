431eb8bf8ccea12922f5c82a34f51c8e07148628
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2022-11-02 16:47:30 -0700

    defining all the category product and tags

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,6 +5,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({ include: [Category, { model: Tag, through: ProductTag }] }).then(categories => res.json(categories)).catch(error => res.status(500).json(error))
   // find all products
   // be sure to include its associated Category and Tag data
 });
